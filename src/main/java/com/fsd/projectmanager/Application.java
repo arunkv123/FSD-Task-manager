0a0a3b537b651ed1b5ac2d5544001b81bc386751
unknown <arunkv.vit@gmail.com>
2019-01-27 08:50:10 +0530

    initial commit

---
+++ src/main/java/com/fsd/projectmanager/Application.java
@@ -0,0 +1,24 @@
+package com.fsd.projectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
